16458b347236975be8a2972e67c767e96285272a
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2023-08-05 20:15:27 -0300

    First commit API

---
+++ api/app/Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+
+import           Web.Spock
+import           Web.Spock.Config
+
+import           Data.Aeson       hiding (json)
+import           Data.Monoid      ((<>))
+import           Data.Text        (Text, pack)
+import           GHC.Generics
+
+data Person = Person
+  { name :: Text
+  , age  :: Int
+  } deriving (Generic, Show)
+
+instance ToJSON Person
+
+instance FromJSON Person
+
+type Api = SpockM () () () ()
+
+type ApiAction a = SpockAction () () () a
+
+main :: IO ()
+main = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 (spock spockCfg app)
+
+app :: Api
+app = do
+  get "people" $ do
+    json [Person { name = "Fry", age = 25 }, Person { name = "Bender", age = 4 }]
