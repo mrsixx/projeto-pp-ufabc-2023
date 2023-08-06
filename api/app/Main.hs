be0a0ab29ee866d2b3c22df7a8dd53a1dddd077b
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2023-08-06 18:19:18 -0300

    feature: Arquitetura da solução (implementação das bibliotecas para persistencia dos dados)

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -1,36 +1,77 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DataKinds #-}
 
 module Main (main) where
 
-
 import           Web.Spock
 import           Web.Spock.Config
-
 import           Data.Aeson       hiding (json)
 import           Data.Monoid      ((<>))
 import           Data.Text        (Text, pack)
 import           GHC.Generics
+import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
+import           Database.Persist        hiding (get) -- To avoid a naming clash with Web.Spock.get
+import qualified Database.Persist        as P         -- We'll be using P.get later for GET /people/<id>.
+import           Database.Persist.Sqlite hiding (get)
+import           Database.Persist.TH
 
-data Person = Person
-  { name :: Text
-  , age  :: Int
-  } deriving (Generic, Show)
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person json -- The json keyword will make Persistent generate sensible ToJSON and FromJSON instances for us.
+  name Text
+  age Int
+  deriving Show
+|]
 
-instance ToJSON Person
+type Api = SpockM SqlBackend () () ()
+type ApiAction a = SpockAction SqlBackend () () a
 
-instance FromJSON Person
+errorJson :: Int -> Text -> ApiAction ()
+errorJson code message =
+  json $
+    object
+    [ "result" .= String "failure"
+    , "error" .= object ["code" .= code, "message" .= message]
+    ]
 
-type Api = SpockM () () () ()
+runSQL :: (HasSpock m, SpockConn m ~ SqlBackend) => SqlPersistT (LoggingT IO) a -> m a
+runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
 
-type ApiAction a = SpockAction () () () a
 
 main :: IO ()
 main = do
-  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  pool <- runStdoutLoggingT $ createSqlitePool "api.db" 5
+  spockCfg <- defaultSpockCfg () (PCPool pool) ()
+  runStdoutLoggingT $ runSqlPool (do runMigration migrateAll) pool
   runSpock 8080 (spock spockCfg app)
+
 
 app :: Api
 app = do
   get "people" $ do
-    json [Person { name = "Fry", age = 25 }, Person { name = "Bender", age = 4 }]
+    allPeople <- runSQL $ selectList [] [Asc PersonId]
+    json allPeople
+  get ("people" <//> var) $ \personId -> do
+    maybePerson <- runSQL $ P.get personId :: ApiAction (Maybe Person)
+    case maybePerson of
+      Nothing -> errorJson 2 "Could not find a person with matching id"
+      Just thePerson -> json thePerson
+  post "people" $ do
+    maybePerson <- jsonBody :: ApiAction (Maybe Person)
+    case maybePerson of
+      Nothing -> errorJson 1 "Failed to parse request body as Person"
+      Just thePerson -> do
+        newId <- runSQL $ insert thePerson
+        json $ object ["result" .= String "success", "id" .= newId]
